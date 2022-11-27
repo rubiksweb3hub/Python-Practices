23d157b00fa117f51173ea3528085dedf8ccbb88
Rubiks Hub <rubiksweb3@gmail.com>
2022-11-27 17:32:20 +0100

    Add files via upload
    A python practice question.

---
+++ practice.py
@@ -0,0 +1,25 @@
+state = input ("What State Are You From?") #Lagos, Ogun, and PortHarcourt
+purchased_amount = int(input('The Amount Purchased ')) #amount purchased
+
+if state == 'Lagos':
+    print (state== 'Lagos')
+    tax_amount = 0.075
+    total_cost = purchased_amount * (1 + tax_amount)
+    result = "since you are from {}, your total cost is {}. ".format (state, total_cost)
+    
+elif state == 'Ogun':
+    print (state== 'Ogun')
+    tax_amount = 0.089
+    total_cost = purchased_amount * (1 + tax_amount)
+    result = "since you are from {}, your total cost is {}. ".format (state, total_cost)
+    
+elif state == 'Port Harcourt':
+    print (state== 'Port Harcourt')
+    tax_amount = 0.095
+    total_cost = purchased_amount * (1 + tax_amount)
+    result = "since you are from {}, your total cost is {}. ".format (state, total_cost)
+    
+else:
+    result=  "select either Lagos, Ogun or Port Harcourt"
+      
+    print(result)
