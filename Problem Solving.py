6807429727774789e85166d71caca1e2b6435411
Rubiks Hub <rubiksweb3@gmail.com>
2022-12-19 00:02:44 +0000

    Pending changes exported from your codespace

---
+++ Problem Solving.py
@@ -0,0 +1,33 @@
+state = input ("What State Are You From?") #Lagos, Ogun, and PortHarcourt
+purchased_amount = int(input('The AmountAmount Purchased ')) #amount purchased
+                                                            
+                                                            else:
+                                                                result=  "select either Lagos, Ogun or Port Harcourt"
+                                                                      
+                                                                          print(result)
+if state == 'Lagos':
+    print (state== 'Lagos')
+        tax_amount = 0.075
+            total_cost = purchased_amount * (1 + tax_amount)
+                result = "since you are from {}, your total cost is {}. ".format (state, total_cost)
+                    
+                    elif state == 'Ogun':
+                        print (state== 'Ogun')
+                            tax_amount = 0.089
+                                total_cost = purchased_amount * (1 + tax_amount)
+                                    result = "since you are from {}, your total cost is {}. ".format (state, total_cost)
+                                        
+                                        elif state == 'Port Harcourt':
+                                            print (state== 'Port Harcourt')
+                                                tax_amount = 0.095
+                                                    total_cost = purchased_amount * (1 + tax_amount)
+                                                        result = "since you are from {}, your total cost is {}. ".format (state, total_cost)
+                                                            
+                                                            else:
+                                                                result=  "select either Lagos, Ogun or Port Harcourt"
+                                                                      
+                                                                          print(resuresultresuresultltresuresultresuresultltltlt)                                                            
+                                                            else:
+                                                                result=  "select either Lagos, Ogun or Port Harcourt"
+                                                                      
+                                                                          print(rAmountrAmountrAmountrAmountrAmountrAmountrAmountrAmount
